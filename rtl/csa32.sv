7675c5751bc45cf642a9e1069419d3227137abb5
Jesus Esparza <67295776+ChutsE@users.noreply.github.com>
2025-05-17 08:28:16 -0600

    Update csa32.sv

--- rtl/csa32.sv
+++ rtl/csa32.sv
@@ -1,11 +1,11 @@
-module csa32 #(parameter N = 3) (
-    input wire [N-1:0] A, B, C,
-    output wire [N-1:0] Co, Sum
+module csa32 #(parameter WIDTH = 3) (
+    input wire [WIDTH-1:0] A, B, C,
+    output wire [WIDTH-1:0] Co, Sum
 );
     genvar i;
-	generate
-		for (i = 0; i < N; i = i + 1) begin : Full_Adder
+    generate
+	for (i = 0; i < WIDTH; i = i + 1) begin : Full_Adder
             sum1b n (.a(A[i]), .b(B[i]), .ci(C[i]), .so(Sum[i]), .co(Co[i]));
-		end
-	endgenerate
+	end
+    endgenerate
 endmodule
